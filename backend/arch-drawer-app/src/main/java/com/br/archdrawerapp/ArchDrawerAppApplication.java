7570b6a89cb5c6b93417bddb20e7e77612508238
Marcelo <marcelo.med.amorim@gmail.com>
2022-08-13 18:28:03 -0300

    adicionados projetos iniciais de frontend e backend

---
+++ backend/arch-drawer-app/src/main/java/com/br/archdrawerapp/ArchDrawerAppApplication.java
@@ -0,0 +1,13 @@
+package com.br.archdrawerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchDrawerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchDrawerAppApplication.class, args);
+	}
+
+}
---
+++ backend/arch-drawer-app/src/test/java/com/br/archdrawerapp/ArchDrawerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.archdrawerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchDrawerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
